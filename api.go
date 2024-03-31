2d02106d37c191ecafab4622bde9391c8cefb285
F. Gau <fgau@gau-net.de>
2024-03-31 21:17:24 +0200

    add StatusCode to log

--- api.go
+++ api.go
@@ -37,6 +37,26 @@ func RequestLoggerMiddleware(next http.Handler) http.HandlerFunc {
 		start := time.Now()
 		next.ServeHTTP(w, r)
 		duration := time.Since(start)
-		log.Printf("method %s, path: %s in %v", r.Method, r.URL.Path, duration)
+
+		lrw := NewLoggingResponseWriter(w)
+		next.ServeHTTP(lrw, r)
+
+		statusCode := lrw.statusCode
+		log.Printf("%s %s %s - %d in %v", r.Method, r.URL.Path, r.Proto, statusCode, duration)
+
 	}
+}
+
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func NewLoggingResponseWriter(w http.ResponseWriter) *loggingResponseWriter {
+	return &loggingResponseWriter{w, http.StatusOK}
+}
+
+func (lrw *loggingResponseWriter) WriteHeader(code int) {
+	lrw.statusCode = code
+	lrw.ResponseWriter.WriteHeader(code)
 }
